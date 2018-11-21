54bccb589f93660916dc1e0db37ceb0e19574c6d
weijun huang <luck938938@hotmail.com>
2018-11-21 03:15:22 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -6,8 +6,14 @@ main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
 
-const input = document.querySelector('input');
+
+
+const input = document.querySelector('input')
  
 input.addEventListener('keydown', function(e) {
-  console.log(e.which);
+  if (e.which === 71) {
+    return e.preventDefault()
+  } else {
+    console.log(e.which)
+  }
 });
